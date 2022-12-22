6dbb1a097220f8a0881a19e93af4cbda3720e977
Kamil <kamil.mlynski1@gmail.com>
2022-12-22 09:13:49 +0100

    timeout fix

--- cypress/e2e/homeTest.cy.ts
+++ cypress/e2e/homeTest.cy.ts
@@ -1,17 +1,13 @@
 /// <reference types="cypress" />
-import {faker} from "@faker-js/faker"
+import { faker } from "@faker-js/faker";
 
+describe("Home page tests", () => {
+  beforeEach(() => {
+    cy.visit("https://www.saucedemo.com");
+  });
 
-describe('Home page tests', () => {
-    beforeEach(() => {
-        cy.visit('https://www.saucedemo.com/')
-    })
-
-    it('should successfully login ', () => {
-        cy.get('[data-test="username"]').type('standard_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-
-        
-    })
-
-})
+  it("should successfully login ", () => {
+    cy.get('[data-test="username"]').type("standard_user");
+    cy.get('[data-test="password"]').type("secret_sauce");
+  });
+});
