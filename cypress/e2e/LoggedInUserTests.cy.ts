c2320dac2684b294627305cde620e34b8c10256a
pyb91 <pyb@spyro-soft.com>
2022-12-27 15:51:19 +0100

    commit5

--- cypress/e2e/LoggedInUserTests.cy.ts
+++ cypress/e2e/LoggedInUserTests.cy.ts
@@ -1,22 +1,20 @@
 /// <reference types="cypress" />
 import { faker } from "@faker-js/faker";
+import loginPage from "../pageObjects/loginPage";
 
 describe("Home page tests", () => {
     beforeEach(() => {
         cy.visit("https://www.saucedemo.com");
+        loginPage.enterUsername('standard_user');
+        loginPage.enterPassword("secret_sauce");
+        loginPage.clickLogin();
     });
 
     it("should successfully login ", () => {
-        cy.get('[data-test="username"]').type("standard_user");
-        cy.get('[data-test="password"]').type("secret_sauce");
-        cy.get('[data-test="login-button"]').click();
         cy.get('.shopping_cart_link');
     });
 
     it("should successfully buy products", () => {
-        cy.get('[data-test="username"]').type("standard_user");
-        cy.get('[data-test="password"]').type("secret_sauce");
-        cy.get('[data-test="login-button"]').click();
         cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
         cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
         cy.get('.shopping_cart_badge').click();
@@ -31,27 +29,17 @@ describe("Home page tests", () => {
     });
 
     it("should successfully display about ", () => {
-        cy.get('[data-test="username"]').type("standard_user");
-        cy.get('[data-test="password"]').type("secret_sauce");
-        cy.get('[data-test="login-button"]').click();
         cy.get('#react-burger-menu-btn').click();
         cy.get('#about_sidebar_link').click();
 
 
     });
-    it.only("should successfully remove from cart ", () => {
-        cy.get('[data-test="username"]').type("standard_user");
-        cy.get('[data-test="password"]').type("secret_sauce");
-        cy.get('[data-test="login-button"]').click();
+    it("should successfully remove from cart ", () => {
         cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
         cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
         cy.get('.shopping_cart_badge').click();
         cy.get('[data-test="remove-sauce-labs-backpack"]').click()
         cy.get('[data-test="remove-sauce-labs-bike-light"]').click()
-
-
-
-
 
     });
 
---
+++ cypress/pageObjects/loginPage.ts
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+import { faker } from "@faker-js/faker";
+
+class loginPage{
+    private loginInput: string = '[data-test="username"]'
+    private loginPasswordInput: string = '[data-test="password"]'
+    private loginButton: string = '[data-test="login-button"]'
+    enterUsername(userName: string){ 
+        cy.get(this.loginInput).type(userName); 
+    
+    
+    }
+    enterPassword(password: string){
+        cy.get(this.loginPasswordInput).type(password);
+    }
+    clickLogin(){
+        cy.get(this.loginButton).click();
+    }
+
+    
+
+}
+export default new loginPage();
