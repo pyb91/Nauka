e59f4d6291ac2c616e53b52c01a72f005d7076f5
pyb91 <pyb@spyro-soft.com>
2023-02-02 15:54:03 +0100

    viewport 2

--- cypress/e2e/LoggedInUserTests.cy.ts
+++ cypress/e2e/LoggedInUserTests.cy.ts
@@ -7,60 +7,71 @@ import productsPage from "../pageObjects/productsPage";
 import headerSection from "../pageObjects/headerSection";
 import footerSection from "../pageObjects/footerSection";
 
+const dimensions = ["samsung-s10", "iphone-x", "macbook-15"];
+
+beforeEach(() => {
+  cy.visit("https://www.saucedemo.com");
+  loginPage.enterUsername("standard_user");
+  loginPage.enterPassword("secret_sauce");
+  loginPage.clickLogin();
+});
 describe("Home page tests", () => {
-  beforeEach(() => {
-    cy.visit("https://www.saucedemo.com");
-    loginPage.enterUsername("standard_user");
-    loginPage.enterPassword("secret_sauce");
-    loginPage.clickLogin();
-  });
+  dimensions.forEach((dimension: Cypress.ViewportPreset) => {
+    it(`should successfully login  - ${dimension}`, () => {
+      cy.viewport(dimension);
+      generalPage.loggedInAssertion();
+    });
 
-  it("should successfully login ", () => {
-    generalPage.loggedInAssertion();
-  });
+    it(`should successfully buy products - ${dimension}`, () => {
+      cy.viewport(dimension);
+      productsPage.addSauceLabsBackpack();
+      productsPage.addSauceLabsBikeLight();
+      headerSection.goToBasket();
+      checkoutSection.clickCheckout();
+      checkoutSection.enterRandomFirstName();
+      checkoutSection.enterRandomLastName();
+      checkoutSection.enterRandomZipCode();
+      checkoutSection.clickContinue();
+      checkoutSection.finishCheckout();
+      checkoutSection.checkIfCheckoutCompleted();
+    });
 
-  it("should successfully buy products", () => {
-    productsPage.addSauceLabsBackpack();
-    productsPage.addSauceLabsBikeLight();
-    headerSection.goToBasket();
-    checkoutSection.clickCheckout();
-    checkoutSection.enterRandomFirstName();
-    checkoutSection.enterRandomLastName();
-    checkoutSection.enterRandomZipCode();
-    checkoutSection.clickContinue();
-    checkoutSection.finishCheckout();
-    checkoutSection.checkIfCheckoutCompleted();
-  });
+    it(`should successfully remove from cart - ${dimension}` , () => {
+      cy.viewport(dimension);
+      productsPage.addSauceLabsBackpack();
+      productsPage.addSauceLabsBikeLight();
+      headerSection.goToBasket();
+      basketPage.removeSauceLabsBackpackFromBasket();
+      basketPage.removeSauceLabsBikeLightFromBasket();
+      basketPage.checkBasketProducts();
+    });
 
-  it("should successfully remove from cart ", () => {
-    productsPage.addSauceLabsBackpack();
-    productsPage.addSauceLabsBikeLight();
-    headerSection.goToBasket();
-    basketPage.removeSauceLabsBackpackFromBasket();
-    basketPage.removeSauceLabsBikeLightFromBasket();
-    basketPage.checkBasketProducts();
-  });
+    it(`should succesfully logout - ${dimension}`, () => {
+      cy.viewport(dimension);
+      generalPage.openHamburgerMenu();
+      generalPage.logOut();
+      loginPage.clickLogin();
+      loginPage.assertLoginFormExists();
+    });
 
-  it("should succesfully logout", () => {
-    generalPage.openHamburgerMenu();
-    generalPage.logOut();
-    loginPage.clickLogin();
-    loginPage.assertLoginFormExists();
-  });
+    it(`footer should display current year - ${dimension}`, () => {
+      cy.viewport(dimension);
+      footerSection.checkFooterYear();
+    });
 
-  it("footer should display current year", () => {
-    footerSection.checkFooterYear();
-  });
+    it(`should open saucelab twitter page - ${dimension}`, () => {
+      cy.viewport(dimension);
+      footerSection.openSocialLinkTwitter();
+    });
 
-  it("should open saucelab twitter page", () => {
-    footerSection.openSocialLinkTwitter();
-  });
+    it(`should open saucelab facebook page - ${dimension}`, () => {
+      cy.viewport(dimension);
+      footerSection.openSocialLinkFacebook();
+    });
 
-  it("should open saucelab facebook page", () => {
-    footerSection.openSocialLinkFacebook();
-  });
-
-  it("should open saucelab linkedin", () => {
-    footerSection.openSocialLinkLinkedin();
+    it(`should open saucelab linkedin - ${dimension}`, () => {
+      cy.viewport(dimension);
+      footerSection.openSocialLinkLinkedin();
+    });
   });
 });
--- cypress/e2e/NotLoggedInUserTests.cy.ts
+++ cypress/e2e/NotLoggedInUserTests.cy.ts
@@ -2,43 +2,31 @@
 import { faker } from "@faker-js/faker";
 import loginPage from "../pageObjects/loginPage";
 
-const dimensions = ["samsung-s10", "iphone-x"];
+const dimensions = ["samsung-s10", "iphone-x", "macbook-15"];
+
+beforeEach(() => {
+  cy.visit("https://www.saucedemo.com");
+});
 
 describe("Home page tests", () => {
-  beforeEach(() => {
-    cy.visit("https://www.saucedemo.com");
-  });
-
       dimensions.forEach((dimension: Cypress.ViewportPreset) => {
-        it("should not allow locked out user to log in", () => {
+        it(`should not allow locked out user to log in - ${dimension}`, () => {
           cy.viewport(dimension);
           loginPage.enterUsername("locked_out_user");
           loginPage.enterPassword("secret_sauce");
           loginPage.clickLogin();
           loginPage.showLockedOutUserLoginErrorMessage();
         });
-        it("should not log in random data", () => {
+        it(`should not log in random data - ${dimension}`, () => {
           cy.viewport(dimension);
           loginPage.enterUsername(faker.name.fullName());
           loginPage.enterPassword(faker.random.word());
           loginPage.clickLogin();
           loginPage.showWrongCredentialsLoginErrorMessage();
         });
+
       });
 
-  
-
-  // it("should not allow locked out user to log in", () => {
-  //   loginPage.enterUsername("locked_out_user");
-  //   loginPage.enterPassword("secret_sauce");
-  //   loginPage.clickLogin();
-  //   loginPage.showLockedOutUserLoginErrorMessage();
-  // });
-
-  // it("should not log in random data", () => {
-  //   loginPage.enterUsername(faker.name.fullName());
-  //   loginPage.enterPassword(faker.random.word());
-  //   loginPage.clickLogin();
-  //   loginPage.showWrongCredentialsLoginErrorMessage();
-  // });
 });
+
+
